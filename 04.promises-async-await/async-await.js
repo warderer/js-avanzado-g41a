1b08e3d3c553a475b1d78ef044ac734ec6808dbd
César Guerra <warderer@gmail.com>
2025-06-12 20:06:13 -0600

    ✨ Se añade ejemplo con Async / Await

--- 04.promises-async-await/async-await.js
+++ 04.promises-async-await/async-await.js
@@ -22,3 +22,25 @@ function getPokemonThen(name = 'ditto') {
 }
 
 getPokemonThen()
+
+// RESOLVER PROMESAS 2: ASYNC AWAIT - Pokéapi
+async function getPokemonAsync(name = 'ditto') {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        }
+
+        const pokemon = await response.json();
+        console.log('Nombre:', pokemon.name);
+        console.log('Altura:', pokemon.height);
+        console.log('Peso:', pokemon.weight);
+        console.log('Habilidades:', pokemon.abilities.map(ability => ability.ability.name).join(', '));
+        return pokemon;
+    } catch (error) {
+        console.error('Error al obtener el Pokémon:', error);
+        throw error;
+    }
+}
+
+getPokemonAsync('pikachu')
