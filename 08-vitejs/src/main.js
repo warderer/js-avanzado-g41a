3665d0c7f491f2254ebbe1c5c0bbcc76e360e0d0
César Guerra <warderer@gmail.com>
2025-06-27 20:06:49 -0600

    ✨ Implemento funcionalidad de la App para llamar a los astronautas

--- 08-vitejs/src/main.js
+++ 08-vitejs/src/main.js
@@ -1,1 +1,25 @@
 import './style.css'
+
+import axios from 'axios'
+import { displayAstronauts } from './displayAstronauts.js'
+
+const API_URL = 'http://api.open-notify.org/astros.json'
+
+const fetchButton = document.getElementById('fetch-button')
+const listContainer = document.getElementById('astronaut-list-container')
+
+const getAstronauts = async () => {
+    try {
+        const response = await axios.get(API_URL)
+        const astronauts = response.data.people
+
+        // Imprimimos todos los nombres de los astronautas
+        displayAstronauts(astronauts, listContainer)
+
+    } catch (error) {
+        console.error('Houston, tenemos un problema. ❌', error.message)
+        listContainer.innerHTML = '<p>Houston, tenemos un problema</p>'
+    }
+}
+
+fetchButton.addEventListener('click', getAstronauts)
